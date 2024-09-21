48c2658268dd3a8ec0c9eebcff6e1dcb88ffead7
Tushar Negi <negitushar81@gmail.com>
2024-09-21 18:22:28 +0530

    can also apply css properties into gsap for seamless animation

--- 01Animations/script.js
+++ 01Animations/script.js
@@ -3,9 +3,11 @@
 // to() method takes 2 parameters first is the element in which the animation should be used, and in the second we can give properties to that element but within curly brackets
 
 gsap.to("#box1", {
-    x: 1000,   // x-axis
-    duration: 2,  // kitne time ke under animation complete hoga(value in seconds)
-    delay: 1,    // animation start kitne time mai hogi(value in seconds)
+    x:1000,   // x-axis
+    duration:2,  // kitne time ke under animation complete hoga(value in seconds)
+    delay:1,    // animation start kitne time mai hogi(value in seconds)
+    rotate:360,
+    backgroundColor:"blue",
 })
 
 
@@ -13,7 +15,9 @@ gsap.to("#box1", {
 
 // parameters are same as the to() method
 gsap.from("#box2", {
-    x: 1000,
-    duration: 2,
-    delay: 1,
+    x:1000,
+    duration:2,
+    delay:1,
+    rotate:360,
+    backgroundColor:"orange",
 })
