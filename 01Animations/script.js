f1ca0cba8aacfd9277a7eed36fa01d7f35adabda
Tushar Negi <negitushar81@gmail.com>
2024-09-21 18:45:01 +0530

    animation properties

--- 01Animations/script.js
+++ 01Animations/script.js
@@ -5,9 +5,9 @@
 // to() method takes 2 parameters first is the element in which the animation should be used, and in the second we can give properties to that element but within curly brackets
 
 gsap.to("#box1", {
-    x:1000,   // x-axis
+    x:1000,      // x-axis
     duration:2,  // kitne time ke under animation complete hoga(value in seconds)
-    delay:1,    // animation start kitne time mai hogi(value in seconds)
+    delay:1,     // animation start kitne time mai hogi(value in seconds)
     rotate:360,
     backgroundColor:"blue",
     borderRadius:"50%",
@@ -36,5 +36,7 @@ gsap.from("h1", {
     delay:1,
     stagger:0.3,   // if you have multiple element with same name like here i have 3 h1 and i want them the text to work one by one, so stagger does the job to animation all the elements with same name one by one
 
-    repeat:1,     // you can repeat the animation by using repeat property, * note the animation will run 2 times because firstly it will run default and form the second time because of the repeat property which says repeat 1 more time
+    repeat:1,      // you can repeat the animation by using repeat property, * note the animation will run 2 times because firstly it will run default and form the second time because of the repeat property which says repeat 1 more time
+
+    yoyo:true,     // if you want the animation to work like yoyo i.e when we through it forward it come back, the yoyo property also does the same thing the animation moves from initial to final and then final to initial in case of yoyo property 
 })
